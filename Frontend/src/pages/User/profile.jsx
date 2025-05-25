6ad25ec0f0be05dacaca3b72bc6bf51dc3a0d4a3
tharunvikram71 <vrajamani741@gmail.com>
2025-05-25 19:40:14 +0530

    new

--- Frontend/src/pages/User/profile.jsx
+++ Frontend/src/pages/User/profile.jsx
@@ -48,9 +48,11 @@ const Profile = () => {
 
   if (!user) {
     return (
-      <div className="d-flex justify-content-center align-items-center vh-100">
-        <div className="spinner-border text-primary" role="status"></div>
-      </div>
+      // <div className="d-flex justify-content-center align-items-center vh-100">
+      //   <div className="spinner-border text-primary" role="status"></div>
+      // </div>
+      <>
+      <div>hai </div></>
     );
   }
 
